6e92b82667117e8a0c3327299d7a82c712c2725b
Sheffchenko <bladdem2017@gmail.com>
2021-10-13 22:34:48 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Todo from './components/Todo';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const onCheck = (id) => {
+    setTodos((todos) => {
+      let arr = [...todos];
+      arr[id] = { ...arr[id], isChecked: !arr[id].isChecked };
+      return arr;
+    });
+  };
+
+  useEffect(() => {
+    console.log(2);
+    let todos = localStorage.getItem('todos');
+    if (todos) {
+      setTodos(JSON.parse(todos));
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+  const onDelete = (id) => {
+    setTodos((todos) => {
+      let arr = [...todos];
+      arr.splice(id, 1);
+      return arr;
+    });
+  };
   return (
     <div className="container">
       <div className="first-block">
         <h1>Todo list:</h1>
         <div className="input-block">
-          <input placeholder="New todo..." className="text" type="text" />
-          <button className="button button--add-todo">New todo</button>
+          <input
+            onChange={(event) => setText(event.target.value)}
+            value={text}
+            placeholder="New todo..."
+            className="text"
+            type="text"
+          />
+          <button
+            onClick={() => {
+              setTodos((todos) => {
+                setText('');
+                return [...todos, { text: text, isChecked: false }];
+              });
+            }}
+            className="button button--add-todo">
+            New todo
+          </button>
         </div>
       </div>
       <div className="todos">
-        <Todo />
-        <Todo />
-        <Todo />
+        {todos
+          .map((el, index) => {
+            return <Todo key={index} {...el} id={index} onCheck={onCheck} onDelete={onDelete} />;
+          })
+          .reverse()}
       </div>
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,27 @@
+import classNames from 'classnames';
 import React from 'react';
-
-const Todo = () => {
+const Todo = ({ text, isChecked, id, onCheck, onDelete }) => {
   return (
     <div className="todo">
-      <input className="checkbox" type="checkbox" />
-      <h3>Make some noize</h3>
-      <button className="delete"></button>
+      <input
+        onChange={() => {
+          onCheck(id);
+        }}
+        checked={isChecked}
+        className="checkbox"
+        type="checkbox"
+        id={'checkbox_' + id}
+      />
+      <label className={classNames({ made: isChecked })} htmlFor={'checkbox_' + id}>
+        {text}
+      </label>
+      <button
+        onClick={() => {
+          return onDelete(id);
+        }}
+        className="delete"></button>
     </div>
   );
 };
 
-export default Todo;
+export default React.memo(Todo);
